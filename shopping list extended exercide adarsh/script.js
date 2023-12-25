de60087089dd922cd829181a2403e265efe89d1e
ADARSH PANDA <91756726+AdarshPanda931@users.noreply.github.com>
2023-12-25 14:09:49 +0530

    Add files via upload

---
+++ shopping list extended exercide adarsh/script.js
@@ -0,0 +1,35 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+//deleting value by clicking the corresponding delete button next to it
+
+
+
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
